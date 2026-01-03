e113af3d2e391d9d10e40b9a0b8d6cb0026c6dd2
piyush ujgaokar <piyushujgaokar544@gmail.com>
2026-01-03 22:17:56 +0530

    set unsplash and pexels api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
+
+  // function getPhotos(){
+  //   fetchPhotos()
+  // }
+
   return (
-    <div className='text-9xl'>App</div>
+    <div className='h-screen w-full bg-gray-950 text-white'>
+      <button onClick={async ()=>{
+        const data=await fetchPhotos('dogs')
+        console.log(data.results);
+        
+      }}>get Photos</button>
+
+      <button className='ml-30' onClick={async ()=>{
+        const data=await fetchVideos('nature')
+        console.log(data.videos);
+        
+      }}>get Videos</button>
+
+    </div>
   )
 }
 
